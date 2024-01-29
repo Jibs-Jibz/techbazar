5acc348c793187db28a33e5867dbd272fa89e402
Ajibola <osunkoyajibola@gmail.com>
2024-01-30 00:36:42 +0100

    update remove fullstop

--- src/components/Sections/FirstSection.tsx
+++ src/components/Sections/FirstSection.tsx
@@ -9,7 +9,7 @@ const FirstSection = () => {
       <section className="md:py-20 py-10 bg-gradient-to-r from gray-00 to-gray-200 spacey-10">
         <div className="container  mx-auto text-center">
           <div className="text-6xl flex justify-center font-bold md:px-20 pb-10 text-gradient bg-gradient-to-r gradient-text bg-clip-text text-transparent ">
-            We are your everyday tech solutions provider .
+            We are your everyday tech solutions provider.
           </div>
           <p className="text-lg md:text-xl md-10 bg-gradient-to-r from-black to-gray-400 dark:from-gray-200 dark:to-gray-400 bg-clip-text text-transparent font-bold ">
             Empowering businesses with specialized IT services for growth and
